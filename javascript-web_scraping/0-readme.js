ef4cac0ed13cf4262fc33099f194ba91785a6934
Nitsu47 <agustraversa2004@gmail.com>
2024-05-13 01:22:18 +0000

    typerror fix

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const filePath = process.argv[2];
 
-fs.readFile(filePath, 'utf8', (error, content) => {
+fs.readFile(filePath, 'utf-8', (error, content) => {
   if (error) {
     console.error(error);
     return;
