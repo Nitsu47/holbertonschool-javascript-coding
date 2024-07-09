7cd4c4727d04be487aea1cb3693a093d544327f8
Nitsu47 <agustraversa2004@gmail.com>
2024-07-09 15:11:43 -0700

    task 1 fix

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -4,7 +4,7 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
   if (name !== null) {
-    console.log(`Your name is: ${name}`);
+    process.stdout.write(`Your name is: ${name}`);
     console.log('This important software is now closing');
     process.exit();
   }
