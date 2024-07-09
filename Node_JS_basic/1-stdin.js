8626e9d49bf9cb4e56813137e6eb9d89c39e55e0
Nitsu47 <agustraversa2004@gmail.com>
2024-07-09 15:57:04 -0700

    task 1 fix

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,8 @@
 console.log('Welcome to Holberton School, what is your name?');
-process.stdin.setEncoding('utf8');
 
-process.stdin.on('readable', () => {
-  const name = process.stdin.read();
-  if (name !== null) {
-    process.stdout.write(`Your name is: ${name}/n`);
-    console.log('This important software is now closing');
-    process.exit();
-  }
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+  console.log(`Your name is: ${name}`);
+  console.log('This important software is now closing');
+  process.exit();
 });
