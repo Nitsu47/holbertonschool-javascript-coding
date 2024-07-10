1b953d5a3f1749d537ffd3c318b8097fdded2271
Nitsu47 <agustraversa2004@gmail.com>
2024-07-10 05:19:35 -0700

    Reads and prints the content of a file

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
-const filepath = process.argv[2];
 
-fs.readFile(filepath, 'utf-8', (err, data) => {
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
-    console.error(err);
-    return;
+    throw err;
   }
   console.log(data);
 });
