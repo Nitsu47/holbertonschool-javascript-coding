44489e3fa9d2504f5800593c14aaaa8d0d8c3073
Nitsu47 <agustraversa2004@gmail.com>
2024-07-09 23:43:19 -0700

    entire rework

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -3,50 +3,58 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs').promises;
 
-async function countStudents(path) {
-  try {
-    const data = await fs.readFile(path, 'utf8');
-    const rows = data.split('\n').filter((row) => row);
-    const headers = rows.shift().split(',');
-    const fieldIndex = headers.indexOf('field');
-    const firstNameIndex = headers.indexOf('firstname');
-    const fields = [...new Set(rows.map((row) => row.split(',')[fieldIndex]))];
+function countStudents(path) {
+  return fs
+    .readFile(path, 'utf8')
+    .then((data) => {
+      const rows = data.split('\n').filter((row) => row);
+      const heads = rows.shift().split(',');
+      const fieldIdx = heads.indexOf('field');
+      const fNameIndex = heads.indexOf('firstname');
+      const fields = [
+        ...new Set(rows.map((row) => row.split(',')[fieldIdx])),
+      ];
+      let result = `Number of students: ${rows.length}\n`;
 
-    let result = `Number of students: ${rows.length}\n`;
-
-    fields.forEach((field) => {
-      const students = rows.filter((row) => row.split(',')[fieldIndex] === field);
-      result += `Number of students in ${field}: ${students.length}. List: ${students.map((student) => student.split(',')[firstNameIndex]).join(', ')}\n`;
+      fields.forEach((field) => {
+        const students = rows.filter(
+          (row) => row.split(',')[fieldIdx] === field
+        );
+        result += `Number of students in ${field}: ${
+          students.length
+        }. List: ${students
+          .map((student) => student.split(',')[fNameIndex])
+          .join(', ')}\n`;
+      });
+      return result;
+    })
+    .catch(() => {
+      throw new Error('Cannot load the database');
     });
-
-    return result;
-  } catch {
-    throw new Error('Cannot load the database');
-  }
 }
+const app = http
+  .createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    const q = url.parse(req.url, true).pathname;
 
-const app = http.createServer(async (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  const path = url.parse(req.url, true).pathname;
-
-  if (path === '/') {
-    res.end('Hello Holberton School!');
-  } else if (path === '/students') {
-    res.write('This is the list of our students\n');
-    try {
-      const data = await countStudents(process.argv[2]);
-      res.end(data);
-    } catch (error) {
-      res.end(error.message);
+    if (q === '/') {
+      res.end('Hello Holberton School!');
+    } else if (q === '/students') {
+      res.write('This is the list of our students\n');
+      countStudents(process.argv[2])
+        .then((data) => {
+          res.write(data);
+          res.end();
+        })
+        .catch((error) => {
+          res.write(error.message);
+          res.end();
+        });
+    } else {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('Not Found');
     }
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not Found');
-  }
-});
-
-app.listen(1245, () => {
-  console.log('Server listening on port 1245');
-});
+  })
+  .listen(1245);
 
 module.exports = app;
