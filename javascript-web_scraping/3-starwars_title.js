e9f51950cd90e585ec1c7aa151e078b73d6be557
Nitsu47 <agustraversa2004@gmail.com>
2024-07-10 05:58:52 -0700

    Task 3 - prints the title of a Star Wars movie where the episode number matches a given integer.

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const request = require('request');
+
+request.get(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, (err, response, body) => {
+  if (err){
+    console.error(err);
+    return;
+  }
+  console.log(JSON.parse(body).title);
+});
