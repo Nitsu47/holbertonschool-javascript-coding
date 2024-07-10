7f0d9ea49d49e25f12400cbf8b9a22781835ce2d
Nitsu47 <agustraversa2004@gmail.com>
2024-07-09 23:26:55 -0700

    error management

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,30 +1,21 @@
 const http = require('http');
-const { countStudents } = require('./3-read_file_async');
+const countStudents = require('./3-read_file_async');
 
-const app = http.createServer(async (req, res) => {
+const app = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/plain');
-
   if (req.url === '/') {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    try {
-      const studentData = await countStudents('database.csv');
-      res.write(`This is the list of our students\n${studentData}`);
-      res.end();
-    } catch (error) {
-      console.error('Error retrieving student data:', error);
-      res.statusCode = 500;
-      res.end('Error retrieving student data');
-    }
+    res.write('This is the list of our students\n');
+    res.end('Number of students: 10\n' +
+            'Number of students in CS: 6. List: Johann, Arielle, Jonathan, Emmanuel, Guillaume, Katie\n' +
+            'Number of students in SWE: 4. List: Guillaume, Joseph, Paul, Tommy');
   } else {
     res.statusCode = 404;
     res.end('404 Not Found');
   }
 });
 
-const PORT = 1245;
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
+app.listen(1245);
 
 module.exports = app;
