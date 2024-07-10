e3106ec84715df91ec1825a16a250d6b852b6a4e
Nitsu47 <agustraversa2004@gmail.com>
2024-07-10 06:00:03 -0700

    style fix

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,7 @@
 const request = require('request');
 
 request.get(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, (err, response, body) => {
-  if (err){
+  if (err) {
     console.error(err);
     return;
   }
